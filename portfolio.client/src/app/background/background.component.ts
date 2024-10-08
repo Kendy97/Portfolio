fff949dda32e38e68276d4c4ee9fa96f71747162
kedzio97 <kedzior299@gmail.com>
2024-10-08 21:21:39 +0200

    08.10.2024

--- portfolio.client/src/app/background/background.component.ts
+++ portfolio.client/src/app/background/background.component.ts
@@ -8,20 +8,19 @@ import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
   templateUrl: './background.component.html',
   styleUrl: './background.component.css'
 })
-export class BackgroundComponent  {
+export class BackgroundComponent implements AfterViewInit {
+  @ViewChild('backgroundCanvas', { static: false }) canvasRef!: ElementRef;
 
+  ngAfterViewInit() {
+    const canvas = this.canvasRef.nativeElement as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d')!;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-
-  constructor() { }
-
-  bubbles = Array.from({ length: 250 }, () => ({
-    style: {
-      top: `${Math.random() * 100}%`,
-      left: `${Math.random() * 100}%`,
-      width: `${Math.random() * 10 + 100}px`,
-      height: `${Math.random() * 10 + 100}px`,
-      animationDelay: `${Math.random() * 10}s`,
-    }
-  }));
- 
+    const draw = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      requestAnimationFrame(draw);
+    };
+    draw();
+  }
 }
