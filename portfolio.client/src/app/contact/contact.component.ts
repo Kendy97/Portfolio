ebc9738c03d3b0df658d8031223ecea1658034bc
kedzio97 <kedzior299@gmail.com>
2024-10-20 15:20:03 +0200

    2024-20-10

--- portfolio.client/src/app/contact/contact.component.ts
+++ portfolio.client/src/app/contact/contact.component.ts
@@ -1,13 +1,38 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SocialComponent } from '../social/social.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgIf, HttpClientModule, FormsModule,SocialComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  constructor(private http: HttpClient) { }
 
+  onSubmit(formData: any) {
+    if (formData.subject && formData.email && formData.message) {
+      const data = {
+        subject: formData.subject,
+        email: formData.email,
+        message: formData.message
+      };
+
+      this.http.post('/api/contact/send-email', data)
+        .subscribe(
+          response => {
+        
+            alert('Wiadomość została wysłana!');
+          },
+          error => {
+       
+            alert('Wystąpił błąd przy wysyłaniu wiadomości. Spróbuj ponownie później.');
+          }
+        );
+    }
+  }
 }
--- portfolio.client/src/app/home/home.component.ts
+++ portfolio.client/src/app/home/home.component.ts
@@ -1,13 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SocialComponent } from '../social/social.component';
+import Typed from 'typed.js';
+import lottie from 'lottie-web';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive,SocialComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent { }
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('lottieContainer') lottieContainer!: ElementRef;
 
+  ngAfterViewInit() {
+
+    const options = {
+      strings: ['Mateusz Kędzierski'],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+      startDelay: 600,
+      loop: true,
+      showCursor: true,
+      cursorChar: '|'
+    };
+    const typed = new Typed('.typed-name', options);
+
+    lottie.loadAnimation({
+      container: this.lottieContainer.nativeElement, 
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: '/animation_1729424211354.json'
+    });
+  }
+}
